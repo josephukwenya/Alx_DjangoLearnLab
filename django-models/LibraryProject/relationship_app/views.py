905543a1ba5e2b1b03020e06c2f44aa4cea35565
Joseph Ukwenya <josephukwenya.ng@gmail.com>
2025-07-19 23:43:36 +0100

    bug fix

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,11 +6,12 @@ from .models import Library
 # Create your views here.
 # Function based view
 def list_books(request):
-    books = Book.objects.select_related('author').all()
-    return render(request, 'list_books.html', {'books': books})
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
 
 # Class based view
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
