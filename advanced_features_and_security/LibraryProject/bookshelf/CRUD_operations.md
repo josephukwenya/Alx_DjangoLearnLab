c3ff71409cc0abb26c2501371ccfb89286ea2172
Joseph Ukwenya <josephukwenya.ng@gmail.com>
2025-08-09 17:52:33 +0100

    feat: advanced authentication

---
+++ advanced_features_and_security/LibraryProject/bookshelf/CRUD_operations.md
@@ -0,0 +1,6 @@
+# CRUD Operations Summary
+
+- [Create Operation](./create.md)
+- [Retrieve Operation](./retrieve.md)
+- [Update Operation](./update.md)
+- [Delete Operation](./delete.md)
---
+++ advanced_features_and_security/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+import relationship_app.views as views
+
+urlpatterns = [
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+    path('admin-view', views.admin_view, name='admin_view'),
+    path('librarian-view', views.librarian_view, name='librarian_view'),
+    path('member-view', views.member_view, name='member_view'),
+    path('books/add_book/', views.add_book, name='add_book'),
+    path('books/<int:pk>/edit_book/', views.edit_book, name='edit_book'),
+    path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),
+]
