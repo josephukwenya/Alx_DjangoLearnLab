9ccd71878ef73654996513677fe02f491e15cc05
Joseph Ukwenya <josephukwenya.ng@gmail.com>
2025-08-15 20:23:44 +0100

    feat: blog task 1

---
+++ django_blog/blog/templates/scripts.js
@@ -0,0 +1,5 @@
+JavaScript File: scripts.js
+// Basic example script to demonstrate dynamic behavior
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Blog page loaded');
+});
